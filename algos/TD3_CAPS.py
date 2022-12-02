627422c79ad79a8d287247263ec91fc58af54b20
Beomyeol Yu <yubeomyeol@gmail.com>
2022-12-02 13:51:45 -0500

    change num of hidden layers

--- algos/TD3_CAPS.py
+++ algos/TD3_CAPS.py
@@ -13,14 +13,15 @@ class Actor(nn.Module):
         super(Actor, self).__init__()
 
         # Fully-Connected (FC) layers
-        self.fc1 = nn.Linear(state_dim,  hidden_dim)
-        self.fc2 = nn.Linear(hidden_dim, hidden_dim)
-        self.fc3 = nn.Linear(hidden_dim, action_dim)
         if larger_net:
             self.fc1 = nn.Linear(state_dim,  hidden_dim)
             self.fc2 = nn.Linear(hidden_dim, hidden_dim)
             self.fc3 = nn.Linear(hidden_dim, hidden_dim)
-            self.fc4 = nn.Linear(hidden_dim, action_dim)        
+            self.fc4 = nn.Linear(hidden_dim, action_dim)
+        else:
+            self.fc1 = nn.Linear(state_dim,  hidden_dim)
+            self.fc2 = nn.Linear(hidden_dim, hidden_dim)
+            self.fc3 = nn.Linear(hidden_dim, action_dim)        
 
         self.min_act = min_act
         self.max_act = max_act
@@ -29,13 +30,20 @@ class Actor(nn.Module):
         
 
     def forward(self, state):
-        action = F.relu(self.fc1(state))
-        action = F.relu(self.fc2(action))
+        if larger_net:
+            action = F.relu(self.fc1(state))
+            action = F.relu(self.fc2(action))
+            action = F.relu(self.fc3(action))
+            action = torch.tanh(self.fc4(action))
+        else:
+            action = F.relu(self.fc1(state))
+            action = F.relu(self.fc2(action))
+            action = torch.tanh(self.fc3(action))
         '''
         # Linear scale, [-1, 1] -> [min_act, max_act] 
         return self.scale_act * torch.tanh(self.fc3(action)) + self.avrg_act
         '''
-        return torch.tanh(self.fc3(action))
+        return action
 
 
 class Critic(nn.Module):
@@ -44,16 +52,6 @@ class Critic(nn.Module):
         """
         Clipped Double-Q Learning:
         """
-
-        # Q1 architecture
-        self.fc1 = nn.Linear(state_dim + action_dim, hidden_dim)
-        self.fc2 = nn.Linear(hidden_dim, hidden_dim)
-        self.fc3 = nn.Linear(hidden_dim, 1)
-
-        # Q2 architecture
-        self.fc4 = nn.Linear(state_dim + action_dim, hidden_dim)
-        self.fc5 = nn.Linear(hidden_dim, hidden_dim)
-        self.fc6 = nn.Linear(hidden_dim, 1)
 
         if larger_net:
             self.fc1 = nn.Linear(state_dim + action_dim, hidden_dim)
@@ -65,17 +63,18 @@ class Critic(nn.Module):
             self.fc6 = nn.Linear(hidden_dim, hidden_dim)
             self.fc7 = nn.Linear(hidden_dim, hidden_dim)
             self.fc8 = nn.Linear(hidden_dim, 1)
+        else:
+            # Q1 architecture
+            self.fc1 = nn.Linear(state_dim + action_dim, hidden_dim)
+            self.fc2 = nn.Linear(hidden_dim, hidden_dim)
+            self.fc3 = nn.Linear(hidden_dim, 1)
 
+            # Q2 architecture
+            self.fc4 = nn.Linear(state_dim + action_dim, hidden_dim)
+            self.fc5 = nn.Linear(hidden_dim, hidden_dim)
+            self.fc6 = nn.Linear(hidden_dim, 1)
 
     def forward(self, state, action):
-        q1 = F.relu(self.fc1(torch.cat([state, action], 1)))
-        q1 = F.relu(self.fc2(q1))
-        q1 = self.fc3(q1)
-
-        q2 = F.relu(self.fc4(torch.cat([state, action], 1)))
-        q2 = F.relu(self.fc5(q2))
-        q2 = self.fc6(q2)
-
         if larger_net:
             q1 = F.relu(self.fc1(torch.cat([state, action], 1)))
             q1 = F.relu(self.fc2(q1))
@@ -86,21 +85,29 @@ class Critic(nn.Module):
             q2 = F.relu(self.fc6(q2))
             q2 = F.relu(self.fc7(q2))
             q2 = self.fc8(q2)
+        else:
+            q1 = F.relu(self.fc1(torch.cat([state, action], 1)))
+            q1 = F.relu(self.fc2(q1))
+            q1 = self.fc3(q1)
+
+            q2 = F.relu(self.fc4(torch.cat([state, action], 1)))
+            q2 = F.relu(self.fc5(q2))
+            q2 = self.fc6(q2)
 
         return q1, q2
 
 
     def Q1(self, state, action):
-        q1 = F.relu(self.fc1(torch.cat([state, action], 1)))
-        q1 = F.relu(self.fc2(q1))
-        q1 = self.fc3(q1)
-
         if larger_net:
             q1 = F.relu(self.fc1(torch.cat([state, action], 1)))
             q1 = F.relu(self.fc2(q1))
             q1 = F.relu(self.fc3(q1))
             q1 = self.fc4(q1)
-        
+        else:
+            q1 = F.relu(self.fc1(torch.cat([state, action], 1)))
+            q1 = F.relu(self.fc2(q1))
+            q1 = self.fc3(q1)        
+
         return q1
 
 
